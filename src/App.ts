0093cb418d17dae6d2d9bdc8ac0583727b37ff66
leesin <lihao7364@163.com>
2023-01-08 23:56:58 +0800

    feat webpack接入file-loader&three添加模型

--- src/App.ts
+++ src/App.ts
@@ -1,34 +1,44 @@
-// import './App.css';
 import * as THREE from 'three';
-import React, { useEffect } from 'react';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import model from './model/scene.gltf';
 
 function App() {
+  // const model = require('./model/scene.gltf');
+  const loader = new GLTFLoader();
+  console.log('loader loaded', model);
+  loader.load(
+    model,
+    function (gltf) {
+      console.log(gltf.scene);
+      scene.add(gltf.scene);
+    },
+    function (xhr) {
+      //侦听模型加载进度
+      console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+    },
+    function (error) {
+      //加载出错时的回调
+      console.log('An error happened');
+    },
+  );
+
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  // const geometry = new THREE.BoxGeometry(1, 1, 1);
+  // const material = new THREE.MeshBasicMaterial({ color: 'blue' });
+  // const cube = new THREE.Mesh(geometry, material);
+  // scene.add(cube);
   camera.position.z = 5;
 
-  function animate() {
+  (function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
     renderer.render(scene, camera);
-  }
-  useEffect(() => {
-    console.log(renderer);
-    animate();
-  }, []);
-  return (
-    <div className="App">
-      <header className="App-header">leesin</header>
-    </div>
-  );
+  })();
 }
 
 export default App;
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <div className="test">111</div>
-    <App />
-  </React.StrictMode>,
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
