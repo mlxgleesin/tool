84332a8c53bb4f61d447483d13c400b309f79c70
leesin <lihao7364@163.com>
2023-01-11 00:54:09 +0800

    feat:pnpm&redux&ts

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,40 @@
  * @desc
  */
 import React, { useEffect, useState, FC, useCallback } from 'react';
-import $ from './index.less';
-// interface Props {
-//   data?: object;
-// }
-/**  */
-const App = props => {
-  useEffect(() => {}, []);
-  return <div className="fs-10">222</div>;
+import { createStore } from 'redux';
+import { Button } from 'antd';
+import Index from '@/pages/index';
+
+const defaultState = 0;
+const reducer = (state = defaultState, action) => {
+  console.log('action', action);
+
+  switch (action.type) {
+    case 'ADD':
+      return state + action.payload;
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+/** App */
+const App = () => {
+  const [name, setName] = useState(1);
+  /** 发送action */
+  const handleDispatchAction = useCallback(() => {
+    store.dispatch({
+      type: 'ADD',
+      payload: 1,
+    });
+    console.log(store.getState());
+  }, []);
+  return (
+    <div className="fs-10">
+      <Button type="primary" onClick={handleDispatchAction}>
+        点击
+      </Button>
+      <Index />
+    </div>
+  );
 };
 export default App;
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * @author @myhexin.com
+ * @date 2023-01-10 23:48:40
+ * @desc
+ */
+import React, { useEffect, useState, FC, useCallback } from 'react';
+import {} from 'antd';
+
+/**  */
+const Index = () => {
+  useEffect(() => {}, []);
+  const index = e => {
+    console.log(e);
+  };
+  return <div className="index">222</div>;
+};
+export default Index;
