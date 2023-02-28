18ac5aafc8fecdbddc7601b6d746a687315a4458
leesin <lihao7364@163.com>
2023-02-28 14:07:34 +0800

    feat:redux

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,21 +1,26 @@
 /**
- * @author @myhexin.com
+ * @author lihao7364@gmail.com
  * @date 2023-01-10 23:48:40
  * @desc
  */
-import React, { useEffect } from 'react';
-import {} from 'antd';
+import React, { useCallback, useState, useEffect } from 'react';
+// import { incrementAsync, increment } from '@/store/store';
+import { Button } from 'antd';
 
 /** 首页 */
 const Index = () => {
+  const [count, setCount] = useState(0);
   useEffect(() => {}, []);
-  // var name = 'windowsName';
-  // (function a() {
-  //   var name = 'Cherry';
-  //   console.log(this);
-  // })();
-  debugger;
-  console.log('outer:' + this);
-  return <div className="index">首页</div>;
+
+  const handleClickCounter = useCallback(() => {
+    setCount(preCount => preCount + 1);
+  }, []);
+
+  return (
+    <div className="index">
+      <div>首页-{count}</div>
+      <Button onClick={handleClickCounter}>计数器</Button>
+    </div>
+  );
 };
 export default Index;
